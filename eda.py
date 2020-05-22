135de1f2e7bf2457fcbd434b778762d1ea8f345a
Scriddie <alexander.reisach@gmail.com>
2020-05-22 23:17:23 +0200

    report bug pytorch

--- eda.py
+++ eda.py
@@ -1,8 +1,83 @@
-import preprocessing
-
-data = preprocessing.load_data("Data/Intra/train/rest_105923_1.h5")
-
+import preprocessing as pp
 import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import itertools
 
-for i in range(10):
-    plt.plot(range(data.shape[1]), data[i, :])
+np.random.seed(0)
+torch.manual_seed(0)
+
+
+class ANN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        # TODO think about how to scale up architecture
+        # self.hidden_dims = [10]
+        self.fc1 = nn.Linear(input_dim, 10)
+        self.fc2 = nn.Linear(10, output_dim)
+        # self.softmax = torch.nn.Softmax(10, output_dim)
+
+    def forward(self, x):
+        output = self.fc1(x)
+        output = self.fc2(output)
+        # output = self.softmax(output)
+        return(output)
+
+# TODO create some dummy data
+x = np.random.random((10, 3))
+# x = np.array([1,2,3]).reshape(1, -1)
+# y = x
+y = x[:, 1].reshape(-1, 1)
+
+x = torch.FloatTensor(x)
+y = torch.FloatTensor(y)
+
+# TODO learn identity function
+ann = ANN(3, 1)
+
+loss_fn = torch.nn.MSELoss(reduction="mean")
+optimizer = torch.optim.Adam(ann.parameters(), lr=1e-3)
+
+losses = []
+n_epochs = 100
+for epoch in range(n_epochs):
+    # TODO use optimizer to update the weights
+    optimizer.zero_grad()
+    output = ann.forward(x)
+    loss = loss_fn(output, y)
+    losses.append(loss.item())
+    loss.backward()
+    optimizer.step()
+
+plt.plot(range(n_epochs), losses)
+plt.show()
+
+
+# TODO get the actual data
+x_tens, y_tens = pp.preprocess()
+
+# TODO define some simple as network and do categorical class predictions
+# ohe class labels, use BCEloss
+
+# find number of classes in data set:
+n_classes = len(np.unique(y_tens))
+ex_label = y_tens[0]
+# ex_label is tensor(3.)
+
+# TODO make sure our labels are a tensor of ints!
+
+labels = torch.tensor([3., 0., 1., 3., 0., 4.])
+
+a = y_tens[0:6].numpy()
+a = torch.tensor(a)
+a = a.unsqueeze(0)
+target = torch.zeros(a.size(0), 15).scatter_(1, a, 1.)
+ex_label = y_tens[0]
+# ex_label is tensor(3.)
+# TODO report pytorch
+target = torch.zeros(1, n_classes).scatter_(0, ex_label, 1.)
+
+labels = torch.tensor([3, 0, 1, 3, 0, 4])
+labels = labels.unsqueeze(0)
+target = torch.zeros(labels.size(0), 15).scatter_(1, labels, 1.)
--- preprocessing.py
+++ preprocessing.py
@@ -18,7 +18,19 @@
 import h5py
 import numpy as np
 import scipy.special as spec
+import os
+import torch
+import itertools
+from sklearn.preprocessing import StandardScaler, scale
+import matplotlib.pyplot as plt
+np.random.seed(0)
+torch.manual_seed(0)
 
+def get_data_paths(type="Intra", train_test="train"):
+    assert type in ["Intra", "Cross"], "Not sure if Intra or Cross"
+    dataset_names = os.listdir(f"Data/{type}/{train_test}")
+    path_names = [os.path.join("Data", type, train_test, i) for i in dataset_names]
+    return path_names
 
 def load_data(path):
     # TODO allow loading of multiple data sets
@@ -30,12 +42,17 @@ def load_data(path):
     return matrix
 
 
-def create_windows(data, window_size):
-    """ slice data into slices of window_size """
+def create_windows(data, window_size, keep_fraction):
+    """ 
+    slice data into slices of window_size 
+    @param keep_fraction: fraction of windows to keep
+    """
     windows = []
-    # TODO how many windows can we form=?
-    # for i in (data.shape[1] - )
-    pass
+    n_possible = data.shape[1] - window_size +1
+    for i in range(n_possible):
+        if np.random.uniform() < keep_fraction:
+            windows.append(data[:, i:i+window_size])
+    return windows
 
 def create_labels(train_data, path):
     """ 
@@ -45,8 +62,61 @@ def create_labels(train_data, path):
         "rest": 0,
         "task_motor": 1,
         "task_story": 2,
-        "task_working_memory": 3
+        "task_story_math": 3,
+        "task_working_memory": 4
     }
-    label = label_dict["".join(path.split("/")[-1].split("_")[:-2])]
-    labels = np.tile(label, train_data.shape[1])
+    label = label_dict["_".join(path.split("/")[-1].split("_")[:-2])]
+    labels = [label] * len(train_data)
     return labels
+
+def scale_obs(obs):
+    return [scale(i, axis=1) for i in obs]
+
+def preprocess(exp_type="Intra", train_test="train", keep_fraction=0.0002, scale_observations=True):
+    # create x of shape (n_batch, n_channel, n_obs)
+    dataset_names = get_data_paths(exp_type, train_test)
+    datasets = [load_data(i) for i in dataset_names]
+
+    all_windows = []
+    all_labels = []
+    for i in range(len(datasets[:])):
+        data_path = dataset_names[i]
+        data = datasets[i]
+        window = create_windows(data, 1000, keep_fraction=keep_fraction)
+        all_windows.append(window)
+        labels = create_labels(window, data_path)
+        all_labels.append(labels)
+    
+    # flatten nested lists
+    window_list = list(itertools.chain.from_iterable(all_windows))
+    label_list = list(itertools.chain.from_iterable(all_labels))
+    
+    # scale each channel in each window
+    if scale_observations:
+        window_list = scale_obs(window_list)
+
+    # shuffle obs and labels equally
+    indices = list(range(len(label_list)))
+    np.random.shuffle(indices)
+    window_list = [window_list[i] for i in indices]
+    label_list = [label_list[i] for i in indices]
+
+    # convert to torch tensors
+    x_tens = torch.FloatTensor(window_list)
+    y_tens = torch.IntTensor(label_list)
+
+    return x_tens, y_tens
+
+def plot_some_obs(x_tens):
+    data = x_tens[0]
+    print(data[0:3, 0:10])
+
+    for i in range(10):
+        plt.plot(range(data.shape[1]), data[i, :])
+    plt.show()
+
+
+if __name__ == "__main__":
+    dataset_names = get_data_paths(type="Intra", train_test="train")
+    datasets = [load_data(i) for i in dataset_names]
+    windows = create_windows(data, 1000)
